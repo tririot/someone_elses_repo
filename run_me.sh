700ee16bf13e3388fd126e433999ae90670628d9
LG <lowell.gould@spgenetics.com>
2019-01-17 12:10:01 -0500

    added logfile

--- run_me.sh
+++ run_me.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 ME=$0
-printf "%-20s: %s\n" "${ME} BEGIN" "`date`"
-printf "%-20s: %s\n" "${ME} END" "`date`"
+LOGFILE="${ME}.log"
+
+printf "%-20s: %s\n" "${ME} BEGIN" "`date`" | tee  $LOGFILE
+printf "%-20s: %s\n" "${ME} END" "`date`" | tee -a $LOGFILE
